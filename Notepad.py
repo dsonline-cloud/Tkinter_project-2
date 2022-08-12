274ec513e7e1d3cf6e214d2189d061ddc97c428b
Deepak Singh <80031692+dsonline-cloud@users.noreply.github.com>
2022-08-12 12:18:36 +0530

    Update Notepad.py

--- Notepad.py
+++ Notepad.py
@@ -16,26 +16,26 @@ def newFile():
 
 def openFile():
     global file
-    file = askopenfilename(defaultextension = "txt", filetypes = [("All File", "*.*"), ("Text Documents")]) #File can be of any type and defualt extension of the file is ".txt". This function will help us in opening the files from the system.
-    #So what will happen above is file will be selected and that file will be assigned to the variable "file" that means all the content of the file will now be stored in the variable "file".
+    file = askopenfilename(defaultextension = "txt", filetypes = [("All File", "*.*"), ("Text Documents")]) #File can be of any type and defualt extension of the file is ".txt". This function will help us in acccessing the files from the system.
+    #So what will happen above is, we will be select a file and that file will be assigned to the variable "file".
 
-    if file == "": #In case there is not file to open, that means we clicked on open but didn't selected any file to open, in that case file will again become "None" that means nothing is there.
+    if file == "": #In case there is not file to open, that means we clicked on open but didn't selected any file to open, in that case file variable will again become "None" that means nothing is there.
         file = None
     else:
-        root.title(os.path.basename(file) + "- Notepad") #It will help us in getting the name of the file only and trimming the path of file and it will replace the title of notepad "untitled - Notepad" with "Name Of File -Notepad" while opening the file.
+        root.title(os.path.basename(file) + "- Notepad") #It will help us in getting the name of the file, it will replace the title of notepad "untitled - Notepad" with "Name Of File -Notepad" while opening the file.
         TextArea.delete(1.0, END) #It will delete all the existing content in the TextArea.
         f = open(file, "r") #Open the file
-        TextArea.insert(1.0, f.read()) #Read the file starting from character 1 which is 1.0 to end of the file and insert that into the TextArea.
+        TextArea.insert(1.0, f.read()) 
         f.close()
         
 
 
 def saveFile():
-    global file #This askesaveasfilename() method opens the dialog box in which we can save the file.
+    global file 
     if file == None: #If it's a new file hence it isn't saved earlier hence "file = None", So now we will save it as a new file.
         file= asksaveasfilename(initialfile = "Untitled.txt",
         defaultextension = ".txt", 
-        filetypes = [("All Files", "*.*"), ("Text Documnets", "*.txt")]) #In case we don't provide any file name so "Untitle -Notepad" will be the default name of the file to save the file.
+        filetypes = [("All Files", "*.*"), ("Text Documnets", "*.txt")]) #In case we don't provide any file name so "Untitled -Notepad" will be the default name of the file to save the file.
          
         if file == "": #If we haven't selected any file to save.
             file = None #It means there is no file.
@@ -84,7 +84,7 @@ if __name__ == '__main__':
     root.wm_iconbitmap("notepad_icon.ico")
     root.geometry("650x750")
     
-    #Adding text area in notepad where we will add text.
+    #Adding text area in notepad where we will be writing the text.
     TextArea = Text(root, font = "lucida 13") #It will create an area we will write the text.
     file = None #It means we haven't opened any file iin notepad yet.
     TextArea.pack(expand = True, fill = BOTH) #Packing the TextArea widget to place the Text area into my root gui window, So we can write in notepad. 
@@ -96,7 +96,7 @@ if __name__ == '__main__':
     FileMenu= Menu(menubar, tearoff = 0)
 
     #To open a new file.
-    FileMenu.add_command(label = "New", command = newFile) #It's a dropdown of File which will appear in menu bar.
+    FileMenu.add_command(label = "New", command = newFile) #It's a dropdown of File menu which will appear in menu bar.
 
     #To open an existing file.
     FileMenu.add_command(label = "Open", command = openFile)
@@ -109,8 +109,6 @@ if __name__ == '__main__':
     FileMenu.add_separator() #Adding a separator. It will add a horizontal line between sub menu "save" and "exit".
     #To close the notepad.
     FileMenu.add_command(label = "Exit", command = quitApp) #It will close the notepad app.
-    
-    #Adding a cascade.
     menubar.add_cascade(label = "File", menu = FileMenu) #All the sub-menu options for the File menu in menu bar will come under this File menu.
     #File Menu Ends.
 
@@ -122,14 +120,13 @@ if __name__ == '__main__':
     EditMenu.add_command(label ="Cut", command = cut) #When you will click on "Cut" sub menu it will call the function named "cut".
     EditMenu.add_command(label ="Copy", command = copy)
     EditMenu.add_command(label ="Paste", command = paste)
-
     menubar.add_cascade(label = "Edit", menu = EditMenu) #It will add the "Edit" menubar in our "menubar object" which we created earlier. So menubar object will simply create a menu bar and this menu "Edit" will be added there.
     #Edit Menu Ends.
 
     #Help menu Starts:
     HelpMenu = Menu(menubar, tearoff = 0) #Creating a menu and fitting it in the same menubar which we created earlier named as "menubar".
     HelpMenu.add_command(label = "About Notepad", command = about)
-    menubar.add_cascade(label= "Help", menu = HelpMenu) #It will place the menu "Help" in the menu bar and it will contain about as it's dropdown/submenu.
+    menubar.add_cascade(label= "Help", menu = HelpMenu) 
     #Help menu ends.
 
     root.config(menu = menubar) #It will create the menu bar and place it according to out defined configuration. That's why it's used in the end only once.
@@ -138,7 +135,7 @@ if __name__ == '__main__':
     #Adding Scrollbar in our Notepad:
     Scroll = Scrollbar(TextArea) #Connecting our text area with scroll bar.
     Scroll.pack(side = RIGHT, fill = Y)
-    Scroll.config(command = TextArea.yview) #Configuring vertical scroll bar, So we move scrollbar vertically.
-    TextArea.config(yscrollcommand = Scroll.set) #Configuring the scrollbar for the TextArea in vertical position because "Scroll.set" will insert/set/place/attach the Scrollbar object "Scroll" which we have created for our TextArea.
+    Scroll.config(command = TextArea.yview) #Configuring vertical scroll bar.
+    TextArea.config(yscrollcommand = Scroll.set) 
 
     root.mainloop()
